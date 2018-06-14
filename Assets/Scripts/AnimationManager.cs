b3db173e1d710156722c0eb4dee0655feee8d256
Nikos <fixergaming@gmail.com>
2018-06-14 22:05:40 +0300

    Replace trigger with collision events to hit the molecules.

--- Assets/Scripts/AnimationManager.cs
+++ Assets/Scripts/AnimationManager.cs
@@ -8,25 +8,27 @@ public class AnimationManager : MonoBehaviour {
         anim = GetComponent<Animator>();
     }
 	
-	void Update () {
+	void FixedUpdate () {
         float h = Input.GetAxis("Horizontal");              // setup h variable as our horizontal input axis
         float v = Input.GetAxis("Vertical");                // setup v variables as our vertical input axis
         anim.SetFloat("Speed", v);                          // set our animator's float parameter 'Speed' equal to the vertical input axis				
         anim.SetFloat("Direction", h);                      // set our animator's float parameter 'Direction' equal to the horizontal input axis	
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnCollisionEnter(Collision other)
     {
-        if (other.tag == "Hydrogen" || other.tag == "Oxygen" || other.tag == "Calcium" || other.tag == "Carbon")
+        string colTag = other.collider.tag;
+        if (colTag == "Hydrogen" || colTag == "Oxygen" || colTag == "Calcium" || colTag == "Carbon")
         {
             GameController controller = FindObjectOfType<GameController>();
-            controller.updateColliderTag(other.tag, other.gameObject);
+            controller.updateColliderTag(colTag, other.gameObject);
         }
     }
 
-    void OnTriggerExit(Collider other)
+    void OnCollisionExit(Collision other)
     {
-        if (other.tag == "Hydrogen" || other.tag == "Oxygen" || other.tag == "Calcium" || other.tag == "Carbon")
+        string colTag = other.collider.tag;
+        if (colTag == "Hydrogen" || colTag == "Oxygen" || colTag == "Calcium" || colTag == "Carbon")
         {
             GameController controller = FindObjectOfType<GameController>();
             controller.updateColliderTag("", other.gameObject);
